804a42168c0624b2d9759eab1be5b2026312d8b4
Somansh <rahulsoni0530@gmail.com>
2022-11-18 13:05:33 +0530

    updation

--- src/data.js
+++ src/data.js
@@ -9,13 +9,13 @@ export default [
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     },
     {
-        title: "Mount Fuji",
+        title: "Haryana",
         location: "Sirsa",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        endDate: "24 Nov, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+        imageUrl: "https://s3-alpha-sig.figma.com/img/3e4c/42db/87935fa3908dfc212c11483e45b24be0?Expires=1669593600&Signature=dGSKxa4PDSMCkbRjpEZP3YfQbJd5aWOHCGO0yQWFfnXsCD5XslDSbtjCXWjUdx8uzW9V1JnRoCYF0zJ7hF3WWjy0FlktkD1n84tWL7pzQChCT0FbSvpWYLN4ut0Dl4MS4i3EnqOcMr5X8ufuYJdQB-jw-AafDC0FIYUj3gOZbO3PSH4iZHlzc7RbBd6J7PpggyHlKGw6XOcmrL5ox7DuN9qa6ykcYuAsI3g8msThqJ9SxRe9Scy3aTmIdzhtdumEjxJx1jFJhV-zGJGjoN0lrC5inMMIk5X-wweOeRyPIMOoy6Tey2-CQfqLZDVET0GTnYJ-XvzyWd0kpLDOzvVqVA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
     },
     {
         title: "Mount Fuji",
